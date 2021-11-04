086ec2dd4f94b39d733c056e44d0abe4ae22af26
LucasGabriello <lucasgda59@gmail.com>
2021-11-04 00:07:14 -0300

    add js modifier to modal html

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,14 +2,34 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //pegar todos os botoes que existem com a class check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     //add escuta
-    button.addEventListener("click" , event => {
-        modal.open()
-    })
+    button.addEventListener("click" ,handleClick)
 })
 
-//pegar quando o marcar como lido for clicado
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event , false))
+}) 
+
+function handleClick(event , check = true){
+    //event.preventDefault()
+    const text = check ? "Marcar como lida":"Excluir"
+
+    modalTitle.innerHTML =`${text} esta pergunta? `
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    modal.open()
+}
