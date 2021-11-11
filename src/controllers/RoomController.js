f720e447ec7669dd6a16fcb24a46aa7ec13c3a86
LucasGabriello <lucasgda59@gmail.com>
2021-11-10 23:18:24 -0300

    configure persistence with SQLite

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,26 @@
+const Database = require("../db/config")
+
 module.exports = {
-    create(req,res){
-        let roomId = 123456
+    async create(req,res){
+        const db = await Database()
+        const pass = req.body.password
+        let roomId =""
+
+        for( var i = 0 ; i < 6 ; i++){
+            roomId += Math.floor(Math.random()*10).toString()
+        }
+
+        console.log(parseInt(roomId))
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+
+        await db.close()
+
         res.redirect(`room/${roomId}`)
     }
 }
